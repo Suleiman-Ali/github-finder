908db0c39767f3ffef7c82f15877d23c5cbef240
Suleiman Ali <alkengjooba@gmail.com>
2022-07-13 17:50:51 +0300

    Refactor

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -3,6 +3,6 @@ import axios from 'axios';
 export default axios.create({
   baseURL: 'https://api.github.com',
   headers: {
-    Authorization: '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',
+    Authorization: 'API_KEY',
   },
 });
